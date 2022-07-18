6baf9b5b62d4b8f542776be8af48154ab4163056
emukupa1 <edward-manda@uhatok.com>
2022-07-18 10:50:56 -0500

    create a key value pair structure

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+typedef struct key_value_pair
+{
+    char key[255];
+    void *value;
+    int type;
+} KeyValuePair;
+
 int main()
 {
-    puts("Hello World");
+    puts("Hello World!");
     return 0;
 }
